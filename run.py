d85923d05f93ddda0b90ec49d33def9ab9561e5d
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-06-01 22:54:28 +0300

    make script to find mean in a row

--- run.py
+++ run.py
@@ -2,14 +2,22 @@ import gspread
 
 gc = gspread.service_account(filename='credentials.json')
 
-# Open a sheet from a spreadsheet in one go
 wks = gc.open("asd").sheet1
 
-# Update a range of cells using the top left corner address
-wks.update([[1, 2], [3, 4]], 'A1')
+grades = []
 
-# Or update a single cell
-wks.update_acell('B42', "it's down there somewhere, let me take another look.")
+row = 2
+coloumn = 5
 
-# Format the header
-wks.format('A1:B1', {'textFormat': {'bold': True}})
+for coloumn in range(5, 9):
+    grades.append(int(wks.cell(row, coloumn).value))
+ 
+mean = sum(grades)/len(grades)
+wks.update_cell(2, 9, mean)
+
+
+
+
+
+
+
