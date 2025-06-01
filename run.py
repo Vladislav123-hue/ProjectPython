2841af85b8c03b962b965a84056268bb6ce92c86
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-06-01 23:12:51 +0300

    make script to find means for every student of the class

--- run.py
+++ run.py
@@ -4,16 +4,16 @@ gc = gspread.service_account(filename='credentials.json')
 
 wks = gc.open("asd").sheet1
 
-grades = []
-
 row = 2
 coloumn = 5
 
-for coloumn in range(5, 9):
-    grades.append(int(wks.cell(row, coloumn).value))
- 
-mean = sum(grades)/len(grades)
-wks.update_cell(2, 9, mean)
+for row in range(2, 11):
+    grades = []
+    for coloumn in range(5, 9):
+        grades.append(int(wks.cell(row, coloumn).value))
+    mean = sum(grades)/len(grades)
+    wks.update_cell(row, 9, mean)
+    grades.clear()
 
 
 
