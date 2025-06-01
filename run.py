a4319f60768aa0985b33c3a291a69bff0b98fddf
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-06-01 17:47:33 +0300

    connect workspace to gs file

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+
+gc = gspread.service_account(filename='credentials.json')
+
+# Open a sheet from a spreadsheet in one go
+wks = gc.open("asd").sheet1
+
+# Update a range of cells using the top left corner address
+wks.update([[1, 2], [3, 4]], 'A1')
+
+# Or update a single cell
+wks.update_acell('B42', "it's down there somewhere, let me take another look.")
+
+# Format the header
+wks.format('A1:B1', {'textFormat': {'bold': True}})
